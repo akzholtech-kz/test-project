d7a6065c6a6a5f1e65b1d0a26e635991eaf9defd
akzholtech-kz <akzhol.nuradin@nu.edu.kz>
2021-08-18 14:23:29 +0600

    components forder has been created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { EdupageComponent } from './edupage/edupage.component';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
-import { PartnersComponent } from './partners/partners.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { EdupageComponent } from './components/edupage/edupage.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { PartnersComponent } from './components/partners/partners.component';
 import { DropDownDirective } from './shared/dropdown.directive';
-import { TimetablesComponent } from './timetables/timetables.component';
+import { TimetablesComponent } from './components/timetables/timetables.component';
 
 @NgModule({
   declarations: [
